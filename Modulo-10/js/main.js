584c10e59a6681886d0beeeb4896e69527510751
nakane <weslleynakane1995@icloud.com>
2024-09-16 17:34:38 +0900

    Add files via upload
    exercício para arrumar alguns erros de digitação no código .
    foi colocado toda parte de script dentro do main.js.
    foi arrumado erros de digitação ,onde estava em português e deveria estar em inglês (*MASCARA = MASK*) (REQUERIDO = RIQUIRED)
    Foi ajustado os links dos <script> do JQuery ,a ordem dos links estavam erradas.

---
+++ Modulo-10/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $("#carousel-imagens").slick({
+        autoplay: true
+    });
+
+    $("#telefone").mask("(00) 00000-0000", {
+        placeholder: "(DDD) 12345-6789"
+    })
+
+    $("#cpf").mask("000.000.000-00", {
+        placeholder: "123.456.789-00"
+    })
+
+    $("#cep").mask("00000-000", {
+        placeholder: "012345-678"
+    })
+
+
+    $("form").validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+        messages: {
+            nome: "Por favor,insira seu nome",
+            email: "Por favor,insira seu E-mail",
+            telefone: "Por favor,insira o numero do seu telefone",
+            endereco: "Por favor,insira o seu endereço",
+            cep: "Por favor,insira o seu CEP",
+            cpf: "Por favor,insira o seu CPF"
+        },
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    });
+});
